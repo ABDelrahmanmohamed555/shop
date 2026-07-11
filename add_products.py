36d990294f9e5db7d5f2fae77281eeb2d03c872a
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-11 08:54:15 +0000

    update

--- add_products.py
+++ add_products.py
@@ -23,7 +23,7 @@ card = f"""
 <div class = "card">
 <img src = "{image}" >
 <h2>{name}</h2>
-<p class = "price">price : {price} EGP </p> 
+<p style = "color : chartreuse" :>price : {price} EGP </p> 
 </div>
 
 <!--add_here-->
