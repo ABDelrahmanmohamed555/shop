9ece58e2ef6715a12c65c774eca3264e51a721e3
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-15 08:24:13 +0000

    scrollbar

--- data/refrigerators.js
+++ data/refrigerators.js
@@ -11,7 +11,7 @@ const refrigerators = {
                 {name :" ماتور 66", price :   "0", image :"refs/GM-66.png"} ,
                 {name :" ماتور 12", price :   "0", image :"refs/GP-12.png"} ,
                 {name :" ماتور 14", price :   "0", image :"refs/GP-14.png"} ,
-                // {name :" شرموطة للايجار", price :   "35 في الساعة (هو اللي هيدفع)", image :"refs/bitch.png"} ,
+                {name :" شرموطة للايجار", price :   "35 في الساعة (هو اللي هيدفع)", image :"refs/bitch.png"} ,
             ]
 
         },
