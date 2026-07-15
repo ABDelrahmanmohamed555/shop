b467bc586c091751690ca0979374da14248413ad
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-15 21:01:08 +0000

    add

--- data/refrigerators.js
+++ data/refrigerators.js
@@ -7,7 +7,7 @@ const refrigerators = {
             name : "ماتور" ,
             products :[
                 {name :"ZMC ماتور مقاس خمس (1/5) ", price :   "0", image :"refs/EMG-77.png" , specs : [ " (ZMC) الماركة : زانوسي","الكود :EMG-77AA" ,"الفريون : 134" ,"شامل الشحن و التركيب ","ضمان سنة معتمد" ,]} ,
-                {name :" ماتور 80", price :   "0", image :"refs/EMG-80.png"} ,
+                {name :"ZMC ماتور مقاس ربع (1/4) ", price :   "0", image :"refs/EMG-80.png", specs : ["(ZMC) الماركة : زانوسي","الكود : EMG-80 AF","الفريون : 134","شامل الشحن و التركيب","ضمان سنة معتمد"]} ,
                 {name :" ماتور 66", price :   "0", image :"refs/GM-66.png"} ,
                 {name :" ماتور 12", price :   "0", image :"refs/GP-12.png"} ,
                 {name :" ماتور 14", price :   "0", image :"refs/GP-14.png"} ,
