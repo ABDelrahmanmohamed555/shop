17ab021c5f2aa10f737c858a29ed70edc6abbaef
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-15 21:18:57 +0000

    add2

--- data/refrigerators.js
+++ data/refrigerators.js
@@ -8,8 +8,8 @@ const refrigerators = {
             products :[
                 {name :"ZMC ماتور مقاس خمس (1/5) ", price :   "0", image :"refs/EMG-77.png" , specs : [ " (ZMC) الماركة : زانوسي","الكود :EMG-77AA" ,"الفريون : 134" ,"شامل الشحن و التركيب ","ضمان سنة معتمد" ,]} ,
                 {name :"ZMC ماتور مقاس ربع (1/4) ", price :   "0", image :"refs/EMG-80.png", specs : ["(ZMC) الماركة : زانوسي","الكود : EMG-80 AF","الفريون : 134","شامل الشحن و التركيب","ضمان سنة معتمد"]} ,
-                {name :" ماتور 66", price :   "0", image :"refs/GM-66.png"} ,
-                {name :" ماتور 12", price :   "0", image :"refs/GP-12.png"} ,
+                {name :"ZMC ماتور مقاس سدس (1/6) ", price :   "0", image :"refs/GM-66.png", specs : ["(ZMC) الماركة : زانوسي","الكود : GM-66 AA","الفريون : 134" ,"شامل الشحن و التركيب" , "ضمان سنة معتمد"]} ,
+                {name :"ZMC ماتور مقاس نصف (1/2)", price :   "0", image :"refs/GP-12.png", specs :["(ZMC) الماركة : زانوسي","الكود : GP-12-TP","الفريون : 134","شامل الشحن و التركيب","ضمان سنة معتمد"]} ,
                 {name :" ماتور 14", price :   "0", image :"refs/GP-14.png"} ,
                 
             ]
