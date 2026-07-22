2f51768160aa0f51f53d2a8c6643c8a54d430b57
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-22 11:14:44 +0000

    products

--- data/microwave.js
+++ data/microwave.js
@@ -2,6 +2,12 @@ const microwave = {
     name : "مايكروويف" ,
     image :"images/categories/microwave.png",
     sections : [
+        {name :"المنتجات",
+            products :[
+                {name :"كارتة مايكروويف جوكر",price:"0",image:"microwave/micro_board.png",specs:["الماركة : XIANG NIU"]}
+            ]
+
+        }
         
     ]
 }
