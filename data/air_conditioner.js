314d75e7771bb2ff5a0c81a57356cf169ba244f1
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-23 10:05:14 +0000

    washers

--- data/air_conditioner.js
+++ data/air_conditioner.js
@@ -5,7 +5,9 @@ const air_conditioner = {
         {
             name:"صحراوي",
             products :[
-                {name:"ترمبة مياه لتكييف صحراوي",price:"0",image:"air_conditioner/pump45.png",specs:["الماركة : SADEK","القوة : 45 واط"]}
+                {name:"ترمبة مياه لتكييف صحراوي",price:"0",image:"air_conditioner/pump45.png",specs:["الماركة : SADEK","القوة : 45 واط"]},
+                {name:"ترمبة مياه لتكييف صحراوي",price:"0",image:"air_conditioner/pump75.png",specs:["الماركة : TOMTHON","القوة : 75 واط"]},
+                {name:"ترمبة مياه لتكييف صحراوي",price:"0",image:"air_conditioner/pump100.png",specs:["الماركة : SADEK","القوة : 100 واط"]},
             ]
         }
     ]
--- data/washing_machine.js
+++ data/washing_machine.js
@@ -2,7 +2,23 @@ const washing_machine = {
     name : " غسالة" ,
     image :"images/categories/washer.png",
     sections :[
-        
+        {
+            name :"مساعدين",
+            products :[
+                {name :"مساعدين توشيبا",price :"0",image :"washers/toshiba8k.png",specs:["الماركة : زعزوع","المقاس 58 سم"]},
+                
+            ]
+        },
+
+
+
+        {
+            name:"ترمبة",
+            products:[
+                {name:"ترمبة طرد مياه",price:"0",image:"washers/diesel_drain.png",specs:["الماركة : DIESEL","القوة : 30 واط"]},
+                {name:"ترمبة طرد مياه",price:"0",image:"washers/speed_drain.png",specs:["الماركة : SPEED","القوة : 30 واط"]},
+            ]
+        }
     ]
 
 }
