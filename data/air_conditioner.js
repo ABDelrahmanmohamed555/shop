afedbbc87dbe40ca9bc53456b1ffa9e660c56f69
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-23 06:55:47 +0000

    HS

--- data/air_conditioner.js
+++ data/air_conditioner.js
@@ -2,7 +2,12 @@ const air_conditioner = {
     name : "تكييف" ,
     image:"images/categories/air_conditioner.png",
     sections :[
-        
+        {
+            name:"صحراوي",
+            products :[
+                {name:"ترمبة مياه لتكييف صحراوي",price:"0",image:"air_conditioner/pump45.png",specs:["الماركة : SADEK","القوة : 45 واط"]}
+            ]
+        }
     ]
 
 }
