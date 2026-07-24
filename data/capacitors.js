3637ed647397d04b2299fc8bac8fee41dd89249f
ABDelrahmanmohamed555 <bdwa39309@gmail.com>
2026-07-24 23:48:34 +0000

    forgot

--- data/capacitors.js
+++ data/capacitors.js
@@ -9,6 +9,7 @@ const capacitors = {
              {name:"مكثف مايكروويف",price:"0",image:"capacitor/microwave_capacitor.png",specs:["الماركة : BICAI","µF السعة : 1.05"]},
              {name:"مكثفات تكييف",price:"0",image:"capacitor/air_cond.png",specs:["الماركة : SADEK-TOMTHON","µF السعة : 35-60"]},
              {name:"مكثفات مراوح",price:"0",image:"capacitor/fan_cond.png",specs:["الماركة : SADEK-DIESEL","µF السعة : 1-5"]},
+             {name:"مكثفات دائمة",price:"0",image:"capacitor/standard.png",specs:["الماركة : SADEK-DIESEL","µF السعة : 5:60"]},
 
             ]
         }
--- data/washing_machine.js
+++ data/washing_machine.js
@@ -13,6 +13,18 @@ const washing_machine = {
 
 
         {
+          name:"جير بوكس",
+          products:[
+
+            {name:"جير بوكس غسالة هاف مقاسات",price:"0",image:"washers/gear_box.png",specs:["الماركة : SADEK-ALABOUDY-DIESEL"]},
+            {name:"جير بوكس غسالة توشيبا 10 كيلو",price:"0",image:"washers/toshiba10k.png",specs:["متوافق مع غسالة توشيبا فوق اوتوماتيك"]},
+            {name:"جير بوكس غسالة توشيبا 8 كيلو",price:"0",image:"washers/toshiba8kG.png",specs:["متوافق مع غسالة توشيبا فوق اوتوماتيك"]},
+          ]
+        },
+
+
+
+        {
             name:"ترمبة",
             products:[
                 {name:"ترمبة طرد مياه",price:"0",image:"washers/diesel_drain.png",specs:["الماركة : DIESEL","القوة : 30 واط"]},
@@ -33,6 +45,9 @@ const washing_machine = {
             name:"اخرى",
             products :[
                 {name:"سير غسالة مقاسات",price:"0",image:"washers/belts.png",specs:["مقاسات : 475-1300"]},
+                {name:"خرطوم صرف ",price:"0",image:"washers/elsakia.png",specs:[" "]},
+                {name:"خرطوم سحب",price:"0",image:"washers/vacuum_.png",specs:[" "]},
+                {name:"كاوتش غسالة حوضين",price:"0",image:"washers/sadek_ruber.png",specs:["الماركة : SADEK"]},
             ]
         }
 
